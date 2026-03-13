d8961f11fc1a5273b3a1a6a4b6b1018597fa29b7
Kavia AI <production@kavia.ai>
2026-03-13 09:24:27 +0000

    Checkpoint 5 - cg4fdb66fc

--- todo_frontend/src/App.test.js
+++ todo_frontend/src/App.test.js
@@ -1,7 +1,23 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 test('renders header', () => {
     render(<App />);
     expect(screen.getByText(/retro todo/i)).toBeInTheDocument();
+});
+
+test('deletes a todo when clicking delete', async () => {
+    const user = userEvent.setup();
+    render(<App />);
+
+    // Initial seed todo from App state
+    expect(screen.getByText('Add your first task')).toBeInTheDocument();
+
+    // Click the first delete button
+    const deleteButtons = screen.getAllByRole('button', { name: /delete todo/i });
+    await user.click(deleteButtons[0]);
+
+    // Todo should be removed
+    expect(screen.queryByText('Add your first task')).not.toBeInTheDocument();
 });
